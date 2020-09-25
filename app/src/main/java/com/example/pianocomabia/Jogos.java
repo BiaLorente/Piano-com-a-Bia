fd4b3b4fcad0f046c56a3d8545fa6fb45474680a
BiaLorente <bia.m.lorente@gmail.com>
2020-09-25 00:20:11 -0300

    Adaptado para ConstraintLayout
    Signed-off-by: BiaLorente <bia.m.lorente@gmail.com>

--- app/src/main/java/com/example/pianocomabia/Jogos.java
+++ app/src/main/java/com/example/pianocomabia/Jogos.java
@@ -7,8 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity  extends AppCompatActivity {
-    private Button piano_button;
+public class Jogos extends AppCompatActivity {
 
 
     @Override
@@ -16,14 +15,7 @@ public class MainActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-        piano_button = (Button) findViewById(R.id.button_piano_id);
-        piano_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Piano.class));
-            }
-        });
-
 
     }
 }
+
